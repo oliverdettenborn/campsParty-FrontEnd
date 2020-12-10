ec6e93266d3edb41e04127f2aeb8bd0df5a5801b
thaliadettenborn <thalia.born@gmail.com>
2020-12-10 12:30:47 -0300

    remove warnings

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,19 @@
 import React, { useState, useContext } from 'react';
 import axios from 'axios';
-
+import { useHistory } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 import { Title, RightBlackBox, PageTwoColumn } from '../../components/';
 import Aside from './Aside';
 import Form from './Form';
 
 export default function SignIn() {
-    const { user, setUser } = useContext(UserContext);
+    const { setUser } = useContext(UserContext);
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
     const [ error, setError ] = useState(null);
     const [ disabledButton , setDisabledButton ] = useState(false);
-
+    const history = useHistory();
+    
     function handleSignIn(e) {
         e.preventDefault();
 
@@ -25,7 +26,7 @@ export default function SignIn() {
         request.then(response => {
             setUser(response.data);
             setDisabledButton(false);
-            //history.push('/participante');
+            history.push('/participante');
         });
 
         request.catch(err => {
