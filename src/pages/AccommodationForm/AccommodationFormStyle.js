b4c8581901376374bc3a19b37cd501a4841b962a
Lucas Murakami <lkenjim98@gmail.com>
2020-12-09 22:48:50 -0400

    Showing modal by clicking in one hotel option

--- src/pages/AccommodationForm/AccommodationFormStyle.js
+++ src/pages/AccommodationForm/AccommodationFormStyle.js
@@ -19,7 +19,6 @@ export const Container = styled.div`
     }
 
     ul {
-        /* background-color: red; */
         flex-grow: 1;
         text-align: center;
     }
@@ -37,18 +36,105 @@ export const Item = styled.li`
     border-radius: 10px;
     cursor: pointer;
     position: relative;
+    box-shadow: 0 0 7px 2px rgba(0, 0, 0, 0.6);
 
     div {
         display: flex;
         justify-content: center;
         align-items: center;
         width: 100%;
-        height: 60%;
+        height: 65%;
         position: absolute;
         bottom: 0;
-        background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 1));
+        background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
         border-radius: 0 0 10px 10px;
         opacity: ${({ isMouseOver }) => isMouseOver ? 1 : 0};
         transition: all 200ms linear;
+    }
+`;
+
+/* Hotel data Modal styles */
+export const modalStyle = {
+    overlay: {
+        'width': '100vw',
+        'height': '100vh',
+        'display': 'flex',
+        'justifyContent': 'center',
+        'alignItems': 'center'
+    },
+    content: {
+        'background': 'none',
+        'border': 'none',
+        'display': 'flex',
+        'justifyContent': 'center',
+        'alignItems': 'center'
+    }
+}
+
+export const ModalContainer = styled.div`
+    background-color: #EFEFEF;
+    display: flex;
+    flex-direction: column;
+    width: 60vw;
+    font-family: 'Lato', 'sans-serif';
+    border-radius: 30px;
+    position: relative;
+
+    img {
+        width: 100%;
+        border-radius: 30px;
+        height: 280px;
+        object-fit: cover;
+        box-shadow: 0 0 7px 1px black;
+    }
+
+    & > div {
+        font-family: 'Chelsea Market', cursive;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 25px;
+        flex-grow: 1;
+
+        & > div:first-child {
+            width: 50%;
+
+            span:first-child { 
+                display: block;
+                font-size: 26px;
+                margin-bottom: 15px;
+                color: gray;
+            }
+        }
+    }
+`;
+
+export const PriceAndButtons = styled.div`
+    div:first-child {
+        width: 160px;
+        text-align: center;
+        background: white;
+        height: 75px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: green;
+        font-size: 26px;
+        border-radius: 15px 15px 0 0;
+    }
+
+    button:first-child {
+        background-color: red;
+        color: white;
+        width: 80px;
+        padding: 10px;
+    }
+
+    button:last-child {
+        background-color: green;
+        color: white;
+        width: 80px;
+        padding: 10px;
     }
 `;
--- src/pages/AccommodationForm/HotelItem.js
+++ src/pages/AccommodationForm/HotelItem.js
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
+import Modal from 'react-modal';
 
-import { Item } from './AccommodationFormStyle';
+import { Item, modalStyle, ModalContainer, PriceAndButtons } from './AccommodationFormStyle';
 
 export default function HotelItem({ hotel }) {
     const [isMouseOver, setIsMouseOver] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
-        <Item
-            backgroundImg={hotel.picUrl}
-            onMouseOver={() => setIsMouseOver(true)}
-            onMouseOut={() => setIsMouseOver(false)}
-            isMouseOver={isMouseOver}
-        >
-            <div>
-                <span>{hotel.name}</span>
-            </div>
-        </Item>
+        <>
+            <Item
+                backgroundImg={hotel.picUrl}
+                onMouseOver={() => setIsMouseOver(true)}
+                onMouseOut={() => setIsMouseOver(false)}
+                isMouseOver={isMouseOver}
+                onClick={() => setIsModalOpen(true)}
+            >
+                <div>
+                    <span>{hotel.name}</span>
+                </div>
+            </Item>
+    
+            <Modal style={modalStyle} isOpen={isModalOpen}>
+                <ModalContainer>
+                    <img src={hotel.picUrl} />
+
+                    <div>
+                        <div>
+                            <span>{hotel.name}</span>
+                            <span>{hotel.description}</span>
+                        </div>
+                        <PriceAndButtons>
+                            <div>
+                                <span>{hotel.price}</span>                                
+                            </div>
+                            
+                            <div>
+                                <button onClick={() => setIsModalOpen(false)}>Cancelar</button>
+                                <button>Confirmar</button>
+                            </div>
+                        </PriceAndButtons>
+                    </div>
+                </ModalContainer>
+            </Modal>
+        </>
     );
 }
