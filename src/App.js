cfcb9fa0d768c9903a609ef84b0db848f5861b62
thaliadettenborn <thalia.born@gmail.com>
2020-12-11 18:07:32 -0300

    adjust

--- src/App.js
+++ src/App.js
@@ -26,23 +26,14 @@ function App() {
           <BrowserRouter>
             <GlobalStyle />
             <Switch>
-            {
-              !finish 
-                ? (
-                    <>
-                    <Route path='/' exact component={ClockPage} />
-                    <Route path='/pre-inscricao' component={SignUp} />
-                    <Route path='/login' component={SignIn} />
-                    <Route path='/participante' exact component={ParticipantPage} />
-                    <Route path='/participante/dados' exact component={FormUser} />
-                    <Route path='/participante/hospedagem' exact component={AccomodationChoosing} />
-                    <Route path='/participante/hospedagem/editar' exact component={AccomodationEdit} />
-                    <Route path='/participante/atividades' exact component={ActivitiesChoosing} />
-                    <Route path='/participante/visualizar-inscricao' exact component={ViewSubscription} />
-                  </>
-                )
-                :  <Route path='/' exact component={ClockPage} />
-            }
+              <Route path='/' component={SignUp} />
+              <Route path='/login' component={SignIn} />
+              <Route path='/participante' exact component={ParticipantPage} />
+              <Route path='/participante/dados' exact component={FormUser} />
+              <Route path='/participante/hospedagem' exact component={AccomodationChoosing} />
+              <Route path='/participante/hospedagem/editar' exact component={AccomodationEdit} />
+              <Route path='/participante/atividades' exact component={ActivitiesChoosing} />
+              <Route path='/participante/visualizar-inscricao' exact component={ViewSubscription} />
             </Switch>
           </BrowserRouter>
         </FormProvider>
